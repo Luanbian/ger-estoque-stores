709c94645e668b984ef24885d10a6ef97a106ae9
luanbianchini <luanbianchine1@gmail.com>
2026-02-16 12:29:27 -0300

    feat: popover shop cart

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,4 +1,11 @@
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
 import { ASSETS_BASE_URL } from "@/constants/assets";
+import { ShoppingCart } from "lucide-react";
 
 interface Props {
   data: {
@@ -12,14 +19,30 @@ export const Header = ({ data }: Props) => {
   const { logo, showName, name } = data;
 
   return (
-    <header className="flex m-2 items-center">
-      <img
-        src={`${ASSETS_BASE_URL}${logo}`}
-        width={80}
-        height={80}
-        alt="Logo"
-      />
-      {showName && <h1 className="text-lg font-bold ml-2">{name}</h1>}
+    <header className="flex m-2 items-center justify-between">
+      <div className="flex items-center">
+        <img
+          src={`${ASSETS_BASE_URL}${logo}`}
+          width={80}
+          height={80}
+          alt="Logo"
+        />
+        {showName && <h1 className="text-lg font-bold ml-2">{name}</h1>}
+      </div>
+      <Popover>
+        <PopoverTrigger asChild className="cursor-pointer">
+          <ShoppingCart />
+        </PopoverTrigger>
+        <PopoverContent className="bg-white rounded-lg p-4">
+          <div>
+            <p className="text-lg font-bold">Carrinho de Compras</p>
+            <p>Seu carrinho está vazio.</p>
+            <Button variant="outline" className="mt-4 cursor-pointer">
+              Concluir
+            </Button>
+          </div>
+        </PopoverContent>
+      </Popover>
     </header>
   );
 };
